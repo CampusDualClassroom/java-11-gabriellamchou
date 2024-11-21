136849bab3582bcab313735c2092dbabf57d2e40
Gabriel <gabriel.lamela.ch@gmail.com>
2024-11-21 12:52:11 +0100

    Ejercicio 10 resuelto

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,25 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+        redRemote.turnOn();
+        redRemote.channelUp();
+        redRemote.channelDown();
+        // Establecer el canal en 0, intentar bajar un canal y comprobar que no se puede
+        System.out.println(redRemote.getChannel());
+        redRemote.channelDown();
+        System.out.println(redRemote.getChannel());
+        redRemote.volumeUp();
+        redRemote.volumeDown();
+        // Establecer el volumen en 0, intentar bajar el volumen y comprobar que no se puede
+        System.out.println(redRemote.getVolume());
+        for (int i = 20; i > 0; i--) {
+            redRemote.volumeDown();
+        }
+        System.out.println(redRemote.getVolume());
+        redRemote.volumeDown();
+        System.out.println(redRemote.getVolume());
+        System.out.println(redRemote.getColor());
+        redRemote.turnOff();
     }
 
 }
--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -1,5 +1,58 @@
 package com.campusdual.classroom;
 
 public class TVRemote {
+    int channel;
+    int volume;
+    boolean on;
+    String color;
 
+    public TVRemote(String color) {
+        this.channel = 0;
+        this.volume = 20;
+        this.color = color;
+    }
+
+    public void turnOn() {
+        this.on = true;
+    }
+
+    public void turnOff() {
+        this.on = false;
+    }
+
+    public void channelUp() {
+        this.channel++;
+    }
+
+    public void channelDown() {
+        if (this.checkMinValue(this.channel)){
+            this.channel--;
+        }
+    }
+
+    public void volumeUp() {
+        this.volume++;
+    }
+
+    public void volumeDown() {
+        if (this.checkMinValue(this.volume)){
+            this.volume--;
+        }
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    private boolean checkMinValue(int value) {
+        return value != 0;
+    }
 }
